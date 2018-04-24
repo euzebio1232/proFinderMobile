ba479e1497ff7f3a126bb8189290e0bfcaf92c92
covadevs <covadevs@gmail.com>
2018-04-23 21:53:59 -0300

    criacao da tela de login e splashscreen

---
+++ app/src/main/java/profinder/com/br/profindermobile/LoginActivity.java
@@ -0,0 +1,13 @@
+package profinder.com.br.profindermobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
