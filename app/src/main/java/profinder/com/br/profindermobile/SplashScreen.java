e6bb485a7145201cc427f8cfa213d1c9f33112ab
PauloHFA <rickphtb@gmail.com>
2018-04-25 22:48:55 -0300

    splashscreen funcionando

--- app/src/main/java/profinder/com/br/profindermobile/SplashScreen.java
+++ app/src/main/java/profinder/com/br/profindermobile/SplashScreen.java
@@ -1,5 +1,7 @@
 package profinder.com.br.profindermobile;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
     }
 }
