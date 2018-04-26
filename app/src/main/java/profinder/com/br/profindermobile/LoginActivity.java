053198f05fd4fcd61b2d945c23a6c1635e242366
PauloHFA <rickphtb@gmail.com>
2018-04-25 22:22:33 -0300

    integração front e back da tela de login

--- app/src/main/java/profinder/com/br/profindermobile/LoginActivity.java
+++ app/src/main/java/profinder/com/br/profindermobile/LoginActivity.java
@@ -2,12 +2,28 @@ package profinder.com.br.profindermobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText mUsuario,mSenha;
+    private Button mLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mUsuario = findViewById(R.id.usuario);
+        mSenha = findViewById(R.id.senha);
+        mLogin = findViewById(R.id.buttonlogin);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
 }
